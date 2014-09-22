63338ee00a42e1950e693bb3ffbf1b3c0316fe06
Mopewa Ogundipe <mopewa.o@gmail.com>
2014-09-22 13:42:26 -0400

    challenge task, allow for goal to be behind robot

--- lab4/part1_test.m
+++ lab4/part1_test.m
@@ -5,7 +5,7 @@ global leftEncoder;
 global rightEncoder;
 global timeStamp;
 
-robot = neato('peta');
+robot = neato('centi');
 
 lh = event.listener(robot.encoders,'OnMessageReceived', ...
     @basicEncoderListener);
@@ -26,30 +26,32 @@ targetX = 1;
 targetY = 0;
 
 kp = 4;
-kd = 0;
+kd = 1; %.025;
 ki = 0;
 
 integralError = 0;
-error = targetX - r.xPos(r.index);
+error = .0039;
 integralError = integralError + error;
 
 integralPos = 0;
 amax = .25 * 3;
 vmax = .25;
 dist = targetX;
-sgn = 1;
 V = 0;
 
 timer = tic;
 currentTime = toc(timer);
 
-% errorPlot = plot(currentTime, error)
+%errorPlot = plot(currentTime, error)
+
+errors = [error];
 
 times = [currentTime];
 xlim([0 5]);
 ylim([-0.2 1.2]);
 
 uref = 0;
+u = 0;
 veloc = 0;
 velocs = [0];
 integralPositions = [0];
@@ -57,9 +59,14 @@ integralPositions = [0];
 encoderStart = leftEncoder;
 encoders = [0];
 
+
+sgn = 1;
+feedforwardOnly = false;
+
 %while(abs(r.xPos(r.index)-targetX) >= 0.0001 || V > .01)
-while (currentTime < (dist + vmax^2/amax)/vmax + 1)
-    
+while ((feedforwardOnly == false && abs(abs(leftEncoder - encoderStart) - (targetX*1000)) >= .0004) ...
+        || (feedforwardOnly == true && currentTime < (dist + vmax^2/amax)/vmax + 1))
+ %while (currentTime < (dist + vmax^2/amax)/vmax + 1)
     eL = leftEncoder - prevLeftEncoder;
     eR = rightEncoder - prevRightEncoder;
     dt = timeStamp - prevTimeStamp;
@@ -67,44 +74,53 @@ while (currentTime < (dist + vmax^2/amax)/vmax + 1)
     prevLeftEncoder = leftEncoder;
     prevRightEncoder = rightEncoder;
     prevTimeStamp = timeStamp;
-
+    
     r = r.updateState(eL, eR, dt);
     
     uref = trapezoidalVelocityProfile(currentTime, amax, vmax, dist, sgn);
     udelay = delayedTrapezoidalVelocityProfile(currentTime, amax, vmax, dist, sgn);
-    integralPos = integralPos + udelay * dt
+    integralPos = integralPos + udelay * dt;
     velocs = [velocs uref];
     integralPositions = [integralPositions integralPos];
     encoders = [encoders (leftEncoder-encoderStart)/1000];
-
-    r.drive(uref, uref);
     
-%     prevError = error;
-%     error = (integralPos - r.xPos(r.index));
-%     integralError = integralError + error * dt;
+    % r.drive(uref, uref);
+    
+    prevError = error;
+    error = (integralPos - r.xPos(r.index));
+    integralError = integralError + error * dt;
+    
+    errors = [errors error];
+    
     currentTime = toc(timer);
     times = [times currentTime];
-%    
-%     V = kp*error + kd*(error-prevError) + ki*integralError;
-%     
-%     if (V > 0.3)
-%         V = 0.3;
-%     end
-%     if (V < -0.3)
-%         V = -0.3;
-%     end
-%     
-%     r.drive(V, V);
+    
+    V = kp*error + kd*(error-prevError) + ki*integralError;
+    
+    if (feedforwardOnly == true)
+        u = uref;
+    else
+        u = V + uref;
+    end
+    
+    if (u > 0.3)
+        u = 0.3;
+    end
+    if (u < -0.3)
+        u = -0.3;
+    end
+    
+    r.drive(u, u);
     
     % plot error for feedback
-%     set(errorPlot,'xdata', [get(errorPlot,'xdata') ...
-%                     currentTime],'ydata', [get(errorPlot,'ydata') ...
-%                     error]);
-%     plot(errorPlot);
-
+    %         set(errorPlot,'xdata', [get(errorPlot,'xdata') ...
+    %                         currentTime],'ydata', [get(errorPlot,'ydata') ...
+    %                         error]);
+    % plot(errorPlot);
+    
     % plot velocity vs position for the simulator
-%     plot(times, velocs, times, integralPositions);
-
+    %     plot(times, velocs, times, integralPositions);
+    
     % plot delayed reference position vs. encoder position
     plot(times, integralPositions, times, encoders);
     
@@ -118,7 +134,7 @@ delete(lh)
 eL = leftEncoder - prevLeftEncoder;
 eR = rightEncoder - prevRightEncoder;
 dt = timeStamp - prevTimeStamp;
-    
+
 prevLeftEncoder = leftEncoder;
 prevRightEncoder = rightEncoder;
 prevTimeStamp = timeStamp;
@@ -126,3 +142,6 @@ prevTimeStamp = timeStamp;
 r = r.updateState(eL, eR, dt);
 
 r.xPos(r.index)
+
+figure(2);
+plot(times, errors);
--- lab4/trapezoidalVelocityProfile.m
+++ lab4/trapezoidalVelocityProfile.m
@@ -2,11 +2,11 @@ function uref = trapezoidalVelocityProfile(t, amax, vmax, dist, sgn)
     t_ramp = vmax/amax;
     t_f = (dist + vmax^2/amax)/vmax;
     if 0 < t && t < t_ramp
-        uref = amax*t;
+        uref = sgn * amax*t;
     elseif 0 < t_f-t && t_f-t < t_ramp
-        uref = amax * (t_f - t);
+        uref = sgn * amax * (t_f - t);
     elseif t_ramp < t && t < t_f-t_ramp
-        uref = vmax;
+        uref = sgn * vmax;
     else
         uref = 0;
     end
